fee8aca909a88f0fc050d73c47d3fa6b1b4aa6e1
scottluskcis <scottlusk.cis@outlook.com>
2019-08-12 22:12:16 -0400

    add application insights to project

--- LoggingAPI/Startup.cs
+++ LoggingAPI/Startup.cs
@@ -25,6 +25,9 @@ namespace LoggingAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // The following line enables Application Insights telemetry collection.
+            services.AddApplicationInsightsTelemetry();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
